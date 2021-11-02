37299c4e41826a3bf5194ebc836be4d534773102
Sampson Orson Jackson <sampson.orson@gmail.com>
2021-11-02 05:51:10 +0100

    fix: change home route component from AppComponent to HomeComponent

--- src/PSR.Angular/projects/psr/src/app/routing/app-routes.ts
+++ src/PSR.Angular/projects/psr/src/app/routing/app-routes.ts
@@ -3,7 +3,7 @@ import { AppComponent } from "@psr/app.component";
 import { HomeComponent } from "../home/home.component";
 
 export const appRoutes: Routes = [
-    { path: 'home', component: AppComponent },
+    { path: 'home', component: HomeComponent },
     {
         path: 'auth',
         loadChildren: () => import("../auth/auth.module").then(m => m.AuthModule)
